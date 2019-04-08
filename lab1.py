06a8139139723dae227bc4324f869a89585f14b1
nastyandreeva <36200697+nastyandreeva@users.noreply.github.com>
2019-04-08 18:47:44 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -6,29 +6,99 @@ data['Pclass'] = data['Pclass'].astype(object)
 
 # 1. Какое количество мужчин и женщин ехало на корабле? В качестве ответа приведите два числа через пробел.
 
+# Реализация без функции:
 sex_counts = data['Sex'].value_counts()
 print("Ответ№1:" , '{} {}'.format(sex_counts['male'], sex_counts['female']))
+
+# Реализация с функцией:
+sex_counts = data['Sex'].value_counts()
+
+def sex_count(sex, data = None):
+    setsex = data.value_counts()
+    if sex == 'male':
+        return setsex['male']
+    else:
+        return setsex['female']
+    
+male_numb = sex_count('male', data['Sex'] )
+female_numb = sex_count('female', data['Sex'])
+print("Ответ№1:" , male_numb, female_numb)
+
+
 # 2. Подсчитайте сколько пассажиров загрузилось на борт в различных портах? Приведите три числа через пробел.
 embarked_counts = data['Embarked'].value_counts()
 print("Ответ№2:" , '{} {} {}'.format(embarked_counts['C'], embarked_counts['S'],embarked_counts['Q']))
+
 # 3. Посчитайте долю погибших на параходе (число и процент)?
+
+# Реализация без функции:
 surv_counts = data['Survived'].value_counts()
 surv_percent = 100.0 * surv_counts[0] / surv_counts.sum()
 print("Ответ№3:", "{:0.2f}".format(surv_percent), surv_counts[0])
+
+# Реализация с функцией:
+surv_counts = data['Survived'].value_counts()
+
+def surv_count(survived, data = None):
+    setsurv = data.value_counts()
+    if survived == 0:
+        return setsurv[0]
+    else:
+        return setsurv[1]
+    
+not_surv_numb = surv_count(0, data['Survived'])
+surv_percent = 100.0 * not_surv_numb / surv_counts.sum()
+print("Ответ№3:" ,not_surv_numb,"{:0.2f}".format(surv_percent))
+
 # 4. Какие доли составляли пассажиры первого, второго, третьего класса?
+
+# Реализация без функции:
 pclass_counts = data['Pclass'].value_counts()
 pclass_percent1 = 100.0 * pclass_counts[1] / pclass_counts.sum()
 pclass_percent2 = 100.0 * pclass_counts[2] / pclass_counts.sum()
 pclass_percent3 = 100.0 * pclass_counts[3] / pclass_counts.sum()
 print("Ответ№4:", "{:0.2f}".format(pclass_percent1), "{:0.2f}".format(pclass_percent2), "{:0.2f}".format(pclass_percent3))
+
+# Реализация с функцией:
+def get_number_of_class(rec):
+    
+  data = pd.read_csv(rec)
+  pclass_percent1 = 100.0 * pclass_counts[1] / pclass_counts.sum()
+  pclass_percent2 = 100.0 * pclass_counts[2] / pclass_counts.sum()
+  pclass_percent3 = 100.0 * pclass_counts[3] / pclass_counts.sum()
+  return pclass_percent1,pclass_percent2,pclass_percent3
+
+get_number_of_class('titanic.csv')
+
 # 5. Вычислите коэффициент корреляции Пирсона между количеством супругов (SibSp) и количеством детей (Parch).
-corr = data['SibSp'].corr(data['Parch'])
-print("Ответ№5:", "{:0.2f}".format(corr))
+def corr_Pirson(x, y):
+    
+    data = pd.read_csv('titanic.csv')
+    res = data[x].corr(data[y])
+    return res
+
+answer = corr_Pirson('SibSp','Parch' )
+print("Ответ№5:", "{:0.2f}".format(answer))
 
 # 6. Выясните есть ли корреляция (вычислите коэффициент корреляции Пирсона) между:
 # возрастом и параметром survival;
 # полом человека и параметром survival;
 # классом, в котором пассажир ехал, и параметром survival.
+
+def corr_Pirson(x, y):
+    
+    data = pd.read_csv('titanic.csv')
+    res = data[x].corr(data[y])
+    return res
+
+answer = corr_Pirson('Age','Survived' )
+print("Ответ№6:", "{:0.2f}".format(answer))
+
+#answer = corr_Pirson('Sex','Survived' )
+#print("Ответ№6:", "{:0.2f}".format(answer))
+
+answer1 = corr_Pirson('Pclass','Survived' )
+print("Ответ№6:", "{:0.2f}".format(answer1))
 
 # 7. Посчитайте средний возраст пассажиров и медиану.
 def age(data = None):
